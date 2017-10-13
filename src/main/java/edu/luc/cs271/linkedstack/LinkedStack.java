286b8dd3d84231e704e48c9fb1caf9cc2a6e93f1
Konstantin Läufer <klaeufer@gmail.com>
2017-10-12 22:25:17 -0500

    fleshed out TODOs
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -9,7 +9,7 @@ public class LinkedStack<E> implements IStack<E> {
   /** The topmost node of this stack. The stack gets pushed down from here. */
   private Node<E> top;
 
-  // TODO why don't we need a constructor?
+  // TODO why don't we need an explicit constructor?
 
   @Override
   public E push(final E obj) {
@@ -37,8 +37,8 @@ public class LinkedStack<E> implements IStack<E> {
 
   @Override
   public List<E> asList() {
-    // TODO: implement using an ArrayList preallocated with the right size
-    // (introduce any instance variable(s) required to support this)
+    // TODO implement using an ArrayList preallocated with the right size
+    // TODO add any instance variable(s) required to support this
     return null;
   }
 }
--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -3,7 +3,18 @@ package edu.luc.cs271.linkedstack;
 public class ReverseLines {
 
 	public static void main(String[] args) {
-		String input;
 		// TODO read successive input lines until EOF, then print out in reverse order
+
+
+
+		String input;
+		while ((input = System.console().readLine()) != null) {
+
+
+
+		}
+
+
+
 	}
 }
