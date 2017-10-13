cb4c7b84be748f088892eb0953196b548407f0b4
Konstantin Läufer <klaeufer@gmail.com>
2017-10-12 19:10:33 -0500

    initial import
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

---
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -0,0 +1,44 @@
+package edu.luc.cs271.linkedstack;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class LinkedStack<E> implements IStack<E> {
+
+  /** The topmost node of this stack. The stack gets pushed down from here. */
+  private Node<E> top;
+
+  // TODO why don't we need a constructor?
+
+  @Override
+  public E push(final E obj) {
+    // TODO
+    return null;
+  }
+
+  @Override
+  public E peek() {
+    // TODO
+    return null;
+  }
+
+  @Override
+  public E pop() {
+    // TODO
+    return null;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    // TODO
+    return false;
+  }
+
+  @Override
+  public List<E> asList() {
+    // TODO: implement using an ArrayList preallocated with the right size
+    // (introduce any instance variable(s) required to support this)
+    return null;
+  }
+}
---
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -0,0 +1,9 @@
+package edu.luc.cs271.linkedstack;
+
+public class ReverseLines {
+
+	public static void main(String[] args) {
+		String input;
+		// TODO read successive input lines until EOF, then print out in reverse order
+	}
+}
