59fa517d95a4333541bc75da85ee22764ba698ad
Konstantin Läufer <laufer@cs.luc.edu>
2017-10-13 09:12:03 -0500

    converted tabs to spaces

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class ReverseLines {
 
-	public static void main(String[] args) {
-		// TODO read successive input lines until EOF, then print out in reverse order
+  public static void main(String[] args) {
+    // TODO read successive input lines until EOF, then print out in reverse order
 
 
 
     final Scanner input = new Scanner(System.in);
-		String line;
-		while ((line = input.nextLine()) != null) {
+    String line;
+    while ((line = input.nextLine()) != null) {
 
 
 
-		}
+    }
 
 
 
-	}
+  }
 }
